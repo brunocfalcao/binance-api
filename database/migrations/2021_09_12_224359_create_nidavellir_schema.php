6d37d6aa498cdf3f54af553ce77a5dd9079bdd7a
Bruno Falcão <bruno.c.falcao@brunofalcao.me>
2021-11-14 01:25:05 +0100

    wip

---
+++ database/migrations/2021_09_12_224359_create_nidavellir_schema.php
@@ -0,0 +1,66 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+use Nidavellir\Database\Seeders\InitialDataSeeder;
+
+class CreateNidavellirSchema extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('exchanges', function (Blueprint $table) {
+            $table->id();
+
+            $table->timestamps();
+            $table->softDeletes();
+
+            $table->engine="MyISAM";
+        });
+
+        Schema::create('tokens', function (Blueprint $table) {
+            $table->id();
+
+            $table->timestamps();
+            $table->softDeletes();
+
+            $table->engine="MyISAM";
+        });
+
+        Schema::create('orders', function (Blueprint $table) {
+            $table->id();
+
+            $table->timestamps();
+            $table->softDeletes();
+
+            $table->engine="MyISAM";
+        });
+
+        Schema::create('alerts', function (Blueprint $table) {
+            $table->id();
+
+            $table->timestamps();
+            $table->softDeletes();
+
+            $table->engine="MyISAM";
+        });
+
+        $seeder = new InitialDataSeeder();
+        $seeder->run();
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('nidavellir_schema');
+    }
+}
